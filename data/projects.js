3512cd40c87cbe1baba077da8118aa90ff953425
N1hron <denik8008@gmail.com>
2024-12-09 04:16:18 +0300

    Change projects order

--- data/projects.js
+++ data/projects.js
@@ -1,48 +1,43 @@
 export default [
     {
-        id: 0,
-        name: 'Graph Editor',
-        link: 'projects/graph-editor/index.html'
-    }, 
-    {
         id: 1,
+        name: 'Marvel DB',
+        link: 'projects/marvel-db/index.html'
+    },
+    {
+        id: 2,
         name: 'Key Press',
         link: 'projects/key-press/index.html'
     },
     {
-        id: 2,
+        id: 3,
+        name: 'Weather App',
+        link: 'projects/weather-app/index.html'
+    },
+    {
+        id: 4,
         name: 'Dev Finder',
         link: 'projects/dev-finder/index.html'
     },
     {
-        id: 3,
+        id: 5,
         name: 'Countries',
         link: 'projects/countries/index.html'
     },
     {
-        id: 4,
+        id: 6,
         name: 'Image Converter',
         link: 'projects/image-converter/index.html'
     },
     {
-        id: 5,
-        name: 'Saasto',
-        link: 'projects/saasto/index.html'
-    },
-    {
-        id: 6,
-        name: 'Loan',
-        link: 'projects/loan/index.html'
-    },
-    {
         id: 7,
-        name: 'Picture',
-        link: 'projects/picture/index.html'
+        name: 'Global Opt',
+        link: 'projects/global-opt/index.html'
     },
     {
         id: 8,
-        name: 'Irvas',
-        link: 'projects/irvas/index.html'
+        name: 'Saasto',
+        link: 'projects/saasto/index.html'
     },
     {
         id: 9,
@@ -51,32 +46,37 @@ export default [
     },
     {
         id: 10,
+        name: 'Graph Editor',
+        link: 'projects/graph-editor/index.html'
+    },
+    {
+        id: 11,
+        name: 'Loan',
+        link: 'projects/loan/index.html'
+    },
+    {
+        id: 12,
+        name: 'Picture',
+        link: 'projects/picture/index.html'
+    },
+    {
+        id: 13,
+        name: 'Irvas',
+        link: 'projects/irvas/index.html'
+    },
+    {
+        id: 14,
         name: 'Uber',
         link: 'projects/uber/index.html'
     },
     {
-        id: 11,
+        id: 15,
         name: 'Clicker',
         link: 'projects/clicker/index.html'
     },
     {
-        id: 12,
-        name: 'Marvel DB',
-        link: 'projects/marvel-db/index.html'
-    },
-    {
-        id: 13,
-        name: 'Global Opt',
-        link: 'projects/global-opt/index.html'
-    },
-    {
-        id: 14,
+        id: 16,
         name: 'Portfolio',
         link: 'projects/portfolio/index.html'
-    },
-    {
-        id: 15,
-        name: 'Weather App',
-        link: 'projects/weather-app/index.html'
     },
 ]
